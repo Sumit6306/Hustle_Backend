990e89bc963878162c8920863368882a88fb2d0a
Sumit6306 <yng.sumit@gmail.com>
2024-09-24 23:43:32 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path= require('path');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine','ejs');
+
+app.get("/", function(req,res) {
+    res.render("index");
+})
+app.get("/profile/:username", function(req,res) {
+    res.send(req.params.username);
+})
+app.get("/profile/:username/:age", function(req,res) {
+    res.send(`Welcome bhodiwaale ${req.params.username} aur tera age ${req.params.age}`)
+})
+app.listen(3000, function(){
+console.log("IT GOOD")
+})
